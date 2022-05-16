03440a2d0d5d5b3ffd847d515182e325e1b54d56
kadizadi <kdzadi@gmail.com>
2022-05-16 18:08:27 +0300

    hi

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css'],
+})
+export class RegistrationComponent implements OnInit {
+  constructor() {}
+  items: MenuItem[];
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Step 1', RouterLink: 'personal' },
+      { label: 'Step 2' },
+      { label: 'Step 3' },
+    ];
+  }
+}
